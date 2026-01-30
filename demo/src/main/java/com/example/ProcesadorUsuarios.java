ec036de2caf922f795f0300db91f374eb5b3a242
Ryad Khaj Jy El Mrabet <ryadkhajjy@gmail.com>
2026-01-30 13:50:42 +0100

    Extracción de métodos estáticos y funciones simples

--- demo/src/main/java/com/example/ProcesadorUsuarios.java
+++ demo/src/main/java/com/example/ProcesadorUsuarios.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,27 +7,40 @@ import java.util.List;
  */
 public class ProcesadorUsuarios {
 
+    private static final int ROL_INVITADO = 2;
+    private static final int ROL_ADMIN = 1;
+
     // Método con 'code smells': largo, números mágicos, malos nombres.
-    public String procesarLista(List<String> dataList) {
+    public String procesarLista(List<String> usuarios) {
         String admins = "";
         String invitados = "";
 
-        for (String u : dataList) {
+        for (String u : usuarios) {
             String[] parts = u.split(":"); // Formato "nombre:rol"
             if (parts.length == 2) {
-                String n = parts[0];
-                int r = Integer.parseInt(parts[1]);
+                String nombre = parts[0];
+                int rol = Integer.parseInt(parts[1]);
 
                 // Número Mágico: 1 es Admin
-                if (r == 1) {
-                    admins += n + ",";
+                if (rol == ROL_ADMIN) {
+                    admins = getAdmins(admins, nombre);
                 }
                 // Número Mágico: 2 es Invitado
-                else if (r == 2) {
-                    invitados += n + ",";
+                else if (rol == ROL_INVITADO) {
+                    invitados = getInvitados(invitados, nombre);
                 }
             }
         }
         return "Admins: " + admins + " | Invitados: " + invitados;
+    }
+
+    private String getInvitados(String invitados, String nombre) {
+        invitados = getAdmins(invitados, nombre);
+        return invitados;
+    }
+
+    private String getAdmins(String admins, String nombre) {
+        admins += nombre + ",";
+        return admins;
     }
 }
